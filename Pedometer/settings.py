31f52e0d6291d1eacdc68bddd52dbbcbddd96070
Evangelos Vlachos <42785341+DoneDeveloper@users.noreply.github.com>
2021-02-27 17:04:49 +0200

    Update settings.py

--- Pedometer/settings.py
+++ Pedometer/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = Path(BASE_DIR).joinpath('staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [Path(BASE_DIR).joinpath('static')]
